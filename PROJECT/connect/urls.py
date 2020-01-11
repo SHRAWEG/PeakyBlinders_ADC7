1615d0c1dc92f96a3d0b94a2b7a63e917ba36872
Jibesh Karanjit <58836714+JibeshK@users.noreply.github.com>
2020-01-12 02:21:15 +0545

    Pushng Branch

--- PROJECT/connect/urls.py
+++ PROJECT/connect/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url
+from .views import search
+ 
 urlpatterns = [
-    
+    path('search/',search),
+
 ]
--- PROJECT/connect/views.py
+++ PROJECT/connect/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Q 
+from .models import student
+from django.http import *
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib import auth
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+
+def search(request):
+    students = student.objects.all()
+    nam = student.name
+    if request.method == 'POST':
+        name = request.POST['srh']
+        if nam == name:
+            return render(request,'searched.html', {'students': students})
+        else:
+            return HttpResponse("no")
+        
+    return render(request,'searched.html',{
+        'students': students
+    })    
+            
